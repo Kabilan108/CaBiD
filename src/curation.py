7728aa6772f29209044897a36b304208d96c3f8b
ApeMaster <tonykabilanokeke@gmail.com>
2022-10-14 23:55:48 -0400

    Add skeleton for CuMiDa class

---
+++ src/curation.py
@@ -0,0 +1,54 @@
+"""
+Data Curation Tools
+
+Authors:  Tony Kabilan Okeke <tonykabilanokeke@gmail.com>
+          Ali Youssef <amy57@drexel.edu>
+          Cooper Molloy <cdm348@drexel.edu>
+
+Purpose:  This module contains functions for data curation.
+"""
+
+# Imports
+
+
+class CuMiDa:
+    """
+    Class for loading datasets from the Curated Microarray Database 
+
+    Attributes
+    ----------
+    INDEX : str
+        Path to the JSON file containing an index of all datasets in the
+        database.
+    BASEURL : str
+        Base URL for downloading datasets from CuMiDa.
+
+
+    Methods
+    -------
+    """
+
+    INDEX = 'data/cumida.json'
+    BASEURL = 'https://sbcb.inf.ufrgs.br'
+
+    def __init__(self):
+        """
+        
+        """
+
+        pass
+
+    def __repr__(self):
+        """
+        
+        """
+
+        pass
+
+    def __str__(self):
+        """
+        
+        """
+
+        pass
+
