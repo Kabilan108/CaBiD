84a70a81aa859e067ecdf655f3b74b9ea0d8fe20
ApeMaster <tonykabilanokeke@gmail.com>
2022-11-01 18:11:01 -0400

    Use drexel email for Tony

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,7 @@
 """
 Utility Functions
 
-Authors:  Tony Kabilan Okeke <tonykabilanokeke@gmail.com>
+Authors:  Tony Kabilan Okeke <tko35@drexel.edu>
           Ali Youssef <amy57@drexel.edu>
           Cooper Molloy <cdm348@drexel.edu>
 
