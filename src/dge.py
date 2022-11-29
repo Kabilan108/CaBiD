236e881365a67427c4e54900e1f07b3cf24bf153
ApeMaster <tonykabilanokeke@gmail.com>
2022-11-29 16:43:27 -0500

    Add dge function

--- src/dge.py
+++ src/dge.py
@@ -1,148 +1,100 @@
 """
-Gene Enrichment Analysis Tools
+CaBiD Differential Gene Expression Analysis
+-------------------------------------------
 
-Authors:  Tony Kabilan Okeke <tonykabilanokeke@gmail.com>
-          Ali Youssef <amy57@drexel.edu>
-          Cooper Molloy <cdm348@drexel.edu>
+This module contains functions for performing differential gene expression
+analysis on the CuMiDa datasets, and generating results tables and figures.
 
-Purpose:  This module contains functions for performing gene enrichment 
-          analysis.
+Author: Tony Kabilan Okeke <tko35@drexel.edu>
+
+Functions & Classes
+-------------------
+sort_normal
+    Sort a pandas series so that normal samples are first
+dge
+    Perform differential gene expression analysis on a GSE dataset
 """
 
+# Import modules
+import seaborn as sns
 import pandas as pd
 import numpy as np
-import requests
+import re
 from statsmodels.stats import multitest
 from scipy import stats
 
-# THESE FUNCTIONS STILL NEED WORK
-# def get_go_info(goid: str) -> dict:
-#     """
-#     Retreive the name and definition of a GO term
-#     """
 
-#     # Rewrite this to use the ensembl API
-#     # https://realpython.com/api-integration-in-python/
+def sort_normal(X: pd.Series):
+    """Sort a Series so that normal samples are first"""
 
-#     if not re.match(r'GO:\d{7}', goid):
-#         raise ValueError('Provide a valid GO identifier.')
+    def sorter(rgx: str, txt: str) -> str:
+        """Sorting key for sorting a list of strings by a regex match"""
+        r = re.search(rgx, txt)
+        return r.group(0) if r else '{'
 
-#     server = "https://rest.ensembl.org/ontology/id/"
-#     r = requests.get(f"{server}{goid}", headers={'Accept': 'application/json'})
-#     r.raise_for_status()
-
-#     return {'name': r.json()['name'], 'descr': r.json()['definition']}
+    return pd.Index([sorter('normal', x) for x in X])
 
 
-# class Enrichment(object):
-#     """
-#     Gene Ontology Enricment
-#     """
+def dge(gse: pd.DataFrame, fc_thr: float=2.0, p_thr: float=0.05):
+    """
+    Perform differential gene expression analysis on a GSE dataset.
+    A Welch's t-test is performed for each gene, and the p-values are corrected
+    for false discovery rate using the Benjamini-Hochberg method.
 
-#     def __init__(self, gpl: pd.DataFrame):
-#         # Create master list of GO terms
-#         self.GO = np.unique([x for sub in gpl.GO.values for x in sub])
-#         self.GO_map = {v:i for i,v in enumerate(self.GO)}
-#         self.GO_map_rev = {i:v for i,v in enumerate(self.GO)}
+    Parameters
+    ----------
+    gse : pd.DataFrame
+        A GSE dataset.
+    fc_thr : float, optional
+        A fold change threshold for differentially expressed genes, by default 2.0
+    p_thr : float, optional
+        A p-value threshold for differentially expressed genes, by default 0.05
+    """
 
-#         # Replace GO terms with numeric identifiers
-#         self.gpl = gpl
-#         self.gpl.GO = self.gpl.GO.apply(lambda x: [self.GO_map[i] for i in x])
+    # Compute fold changes and p-values
+    dge = (gse.groupby('SAMPLE_TYPE').mean()
+        .sort_index(key=sort_normal))
+    fc = dge.iloc[0,:] - dge.iloc[1,:]  # Subtract because log transformed
+    pvals = stats.ttest_ind(
+        gse.filter(regex='normal', axis=0),
+        gse.filter(regex='^((?!normal).)*$', axis=0),
+        equal_var=False
+    )
 
-#         # Retrieve all GO terms
-#         self.go_all = np.array([i for sub in self.gpl.GO for i in sub])
-#         self.M = self.gpl.GO.shape[0]  # HGM Prameter
+    # P-value correction
+    adj_pvals = multitest.fdrcorrection(pvals.pvalue)[1]
 
-#     def dge(self, gse: pd.DataFrame, group_col: str='type', p_thr: float=0.05, 
-#             fc_thr: float=1.5) -> tuple:
-#         """
-#         Differential Gene Expression 
-#         Identify and filter out differentially expressed genes in a GSE
-#         This function assumes expression values are log-transformed.
+    # Create dge table
+    dge = pd.DataFrame({
+        'fc': fc,
+        'pval': pvals.pvalue,
+        'adj pval': adj_pvals,
+        '-log10(adj pval)': -np.log10(adj_pvals),
+        'de': (adj_pvals < p_thr) & (abs(fc) > fc_thr)
+    }).reset_index().rename(columns={'index': 'gene'})
+    dge['de'] = dge['de'].replace({True: 'Significant', False: 'Non Significant'})
 
-#         @param gse: pd.DataFrame
-#             Expression data frame with features (genes) as columns and
-#             samples as rows; any non-numeric columns should be in the 
-#             data frame index
-#         @param gpl: pd.DataFrame
-#             Data frame with Gene IDs and lists of enriched GO terms
-#         @param group_col: str
-#             Column containing group (sample) labels; 
-#             GSE should contain only 2 groups
-#         @param p_thr: float
-#             significance threshold 
-#         @param fc_thr: float
-#             fold change threshold
-#         @return: tuple
-#             list of significant genes, and Series of fold change values
-#         """
+    return dge
 
-#         # Filter out any genes that aren't in the gpl 
-#         # (ones that do not correspond to GO terms)
-#         gse = gse.loc[:, gse.columns.isin(self.gpl.ID)]
 
-#         # Compute fold change and p-values
-#         deg = gse.groupby(group_col).mean()
-#         groups = gse.index.get_level_values(group_col).unique()
-#         if len(groups) > 2:
-#             raise ValueError(f"gse['{group_col}'] should contain only 2 groups")
-#         fc = deg.loc[groups[0], :] / deg.loc[groups[1], :]
-#         pvals = stats.ttest_ind(
-#             gse.filter(regex=groups[0], axis=0),
-#             gse.filter(regex=groups[1], axis=0)
-#         ).pvalue
-#         qvals = multitest.fdrcorrection(pvals)[1]  # FDR Correction
+def plot_volcano(ax, dge):
+    """
+    Create a volcano plot
+    """
 
-#         # Create list of significantly differentially expressed genes
-#         I = (fc.values > fc_thr) | (fc.values < 1/fc_thr) & (qvals < p_thr)
-#         sig_genes = gse.columns[I].to_numpy()
-
-#         return sig_genes, fc
-
-#     def get_hgm_params(self, go_id: int) -> tuple:
-#         """
-#         Return parameters for a hypogeometric test for a specific go term
-#             x: Number of significant genes that are in this term
-#             M: total number of genes
-#             n: number of genes (significant or not) that are in this term
-#             N: total number of significant genes (ignore those with no GO term)
-#         @param go_sig: np.array
-#             List of significant GO Terms
-#         @param go_id: int
-#             Integer corresponding to a GO Term (mapped with self.GO_map)
-#         @return: tuple
-#             Parameters for the hypergeometric test
-#         """
-
-#         x = (self.go_sig == go_id).sum()
-#         n = (self.go_all == go_id).sum()
-#         return x, self.M, n, self.N
-
-#     def hgm_tests(self, sig_genes) -> pd.DataFrame:
-#         """
-#         Perform Hypergeometric Tests for GO enrichment
-#         """
-
-#         # Retrieve list of GO terms for significant genes
-#         sig = self.gpl[self.gpl.ID.isin(sig_genes)].GO
-#         self.go_sig = np.array([i for sub in sig for i in sub])
-#         self.N = sig.shape[0]  # HGM Parameter
-
-#         # Create list of GO terms to test for
-#         candidates = np.unique(self.go_sig)
-#         self.candidates = candidates
-
-#         # Create dataframe for storing results
-#         go_tests = pd.DataFrame(candidates)
-#         go_tests['GO Term'] = list( map(self.GO_map_rev.get, candidates) )
-        
-#         # Hypergeometric Test
-#         go_tests['params'] = go_tests[0].apply(self.get_hgm_params)
-#         go_tests['p-value'] = go_tests['params'] \
-#             .apply(lambda x: stats.hypergeom.sf(*x))
-#         go_tests['q-value'] = multitest.fdrcorrection(go_tests['p-value'])[1]
-#         go_tests.drop([0, 'params'], axis=1, inplace=True)
-#         go_tests = go_tests[ go_tests['q-value'] < .01 ].reset_index(drop=True)
-
-#         return go_tests
-
+    ax.clear()
+    sns.scatterplot(
+        data=dge, x='fc', y='-log10(adj pval)', hue='de',
+        ax=ax, s=20, alpha=0.5, palette=['#999999', '#ff0000']
+    )
+    ax.axhline(-np.log10(0.05), color='#999999', linestyle='--')
+    ax.axvline(2, color='#999999', linestyle='--')
+    ax.axvline(-2, color='#999999', linestyle='--')
+    sns.move_legend(
+        ax, "lower center", ncol=3, title=None,
+        frameon=False, bbox_to_anchor=(.5, 1),
+    )
+    ax.grid()
+    ax.set_xlabel('Fold Change')
+    ax.set_ylabel('-log10(adj pvalue)')
+    sns.despine()
